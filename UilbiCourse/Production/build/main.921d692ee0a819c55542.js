b0ef0c5c71331e49c3942222b16359871cf221a5
HolidayTime <araragikyn@gmail.com>
2024-12-09 10:22:42 +0800

    add some config properties

--- UilbiCourse/Production/build/main.921d692ee0a819c55542.js
+++ UilbiCourse/Production/build/main.921d692ee0a819c55542.js
@@ -10,23 +10,23 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/index.js":
+/***/ "./src/index.ts":
 /*!**********************!*\
-  !*** ./src/index.js ***!
+  !*** ./src/index.ts ***!
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _test__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./test */ \"./src/test.js\");\n\n\n(0,_test__WEBPACK_IMPORTED_MODULE_0__.TestFunc)(\"hi zyabls baby bon\");\n\n\n//# sourceURL=webpack://production/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _test__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./test */ \"./src/test.ts\");\n\n(0,_test__WEBPACK_IMPORTED_MODULE_0__.TestFunc)(\"hi zyabls baby bon\");\n\n\n//# sourceURL=webpack://production/./src/index.ts?");
 
 /***/ }),
 
-/***/ "./src/test.js":
+/***/ "./src/test.ts":
 /*!*********************!*\
-  !*** ./src/test.js ***!
+  !*** ./src/test.ts ***!
   \*********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"TestFunc\": () => (/* binding */ TestFunc)\n/* harmony export */ });\nfunction TestFunc(message){\n    console.log(message);\n}\n\n//# sourceURL=webpack://production/./src/test.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   TestFunc: () => (/* binding */ TestFunc)\n/* harmony export */ });\nfunction TestFunc(message) {\n    console.log(message);\n    return 1;\n}\n\n\n//# sourceURL=webpack://production/./src/test.ts?");
 
 /***/ })
 
@@ -90,7 +90,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.ts");
 /******/ 	
 /******/ })()
 ;
