f1c070bf892b2758c0129e0961781d08e3d0c56a
HolidayTime <araragikyn@gmail.com>
2024-12-19 15:12:50 +0800

    add lazy loading for pages (split bundle to chunks)

--- UilbiCourse/Production/src/App.tsx
+++ UilbiCourse/Production/src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { CounterComponent } from "./components/Counter";
 import "./index.scss";
 import { Route, Routes } from "react-router-dom";
-import AboutPage from "./Pages/AboutPage/AboutPage";
-import MainPage from "./Pages/MainPage/MainPage";
+import { MainPageAsync } from "./Pages/MainPage/MainPage.async";
 import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./Pages/AboutPage/AboutPage.async";
 
 const App = () => {
   return (
@@ -12,10 +12,12 @@ const App = () => {
       Some component after this text.
       <Link to={"/"}>-Main Page-</Link>
       <Link to={"/about"}>-About-</Link>
-      <Routes>
-        <Route path={"/about"} element={<AboutPage />} />
-        <Route path={"/"} element={<MainPage />} />
-      </Routes>
+      <Suspense fallback={<div>0_0</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
