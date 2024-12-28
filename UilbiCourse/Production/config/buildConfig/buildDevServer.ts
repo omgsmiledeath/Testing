934553ac6f79eb723d70ba04adda4557d6554076
HolidayTime <araragikyn@gmail.com>
2024-12-28 15:19:50 +0800

    add Hot Replacement and React refresh modules

--- UilbiCourse/Production/config/buildConfig/buildDevServer.ts
+++ UilbiCourse/Production/config/buildConfig/buildDevServer.ts
@@ -5,5 +5,6 @@ export function buildDevServer(options: BuildOptions): DevConfiguration {
     port: options.port,
     open: true,
     historyApiFallback: true,
+    hot: true,
   };
 }
--- UilbiCourse/Production/config/buildConfig/buildPlugins.ts
+++ UilbiCourse/Production/config/buildConfig/buildPlugins.ts
@@ -2,12 +2,12 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
-
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  let plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -20,4 +20,9 @@ export function buildPlugins({
       __IS_DEV__: JSON.stringify(isDev),
     }),
   ];
+  if (isDev) {
+    plugins.push(new ReactRefreshWebpackPlugin());
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
+  return plugins;
 }
